11085821c95a7d7d4daa06489bbc306122df6c46
Bronstrom <bafellstrom@gmail.com>
2024-02-19 21:44:04 -0500

    Add top track time range dropdown

--- spotilight-vue/src/components/TopTracks.vue
+++ spotilight-vue/src/components/TopTracks.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="top-tracks m-5">
     <h3>Top Tracks</h3>
+    <div class="dropdown">
+      <button
+        class="btn btn-primary dropdown-toggle"
+        type="button"
+        id="top-track-frequency-button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Dropdown button
+      </button>
+      <div class="dropdown-menu" aria-labelledby="top-track-frequency-button">
+        <a
+          class="dropdown-item"
+          @click="(this.curTopTracks = []), changeTimeRange('short_term')"
+          >Short</a
+        >
+        <a
+          class="dropdown-item"
+          @click="(this.curTopTracks = []), changeTimeRange('medium_term')"
+          >Medium</a
+        >
+        <a
+          class="dropdown-item"
+          @click="(this.curTopTracks = []), changeTimeRange('long_term')"
+          >Long</a
+        >
+      </div>
+    </div>
     <div
       class="track row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 justify-content-center"
     >
       <div
         class="track col"
-        v-for="(track, index) in topTracks?.items"
-        v-bind:key="track.id - index"
+        v-for="track in curTopTracks"
+        v-bind:key="track.id"
       >
         <div class="track card h-100">
-          {{ console.log(track) }}
+          <!--{{ console.log(track) }}-->
           <img
             class="track card-image"
             :src="track.album.images[0].url"
             :alt="track.album.name + ' album cover'"
           />
-          {{ console.log(track?.album?.artists) }}
           <div class="track card-body">
             <h5 class="track card-title">{{ track.name }}</h5>
             <h6 class="track card-album">
@@ -28,7 +55,7 @@
               <span
                 class="track card-text"
                 v-for="(artist, index) in track?.artists"
-                :key="artist - index"
+                :key="artist"
               >
                 {{ index === 0 ? "" : ", " }}
                 {{ artist.name }} ({{ artist.type }})
@@ -62,26 +89,70 @@ export default {
   name: "TopTracks",
   data() {
     return {
-      topTracks: "",
+      topTracksShort: [],
+      topTracksMedium: [],
+      topTracksLong: [],
+      curTopTracks: [],
     };
   },
   methods: {
     handleTopTracks() {
-      const profile_endpoint = `https://api.spotify.com/v1/me/top/tracks?offset=0&limit=${TOP_COUNT}`;
+      // TODO: Refactor some of this duplication
+      const medium_term_track_endpoint = `https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&offset=0&limit=${TOP_COUNT}`;
       axios({
         method: "GET",
-        url: profile_endpoint,
+        url: medium_term_track_endpoint,
         headers: getHeader(),
       })
         .then((res) => {
-          this.topTracks = res.data;
+          this.topTracksMedium = res.data.items;
+          this.curTopTracks = res.data.items;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      const short_term_track_endpoint = `https://api.spotify.com/v1/me/top/tracks?time_range=short_term&offset=0&limit=${TOP_COUNT}`;
+      axios({
+        method: "GET",
+        url: short_term_track_endpoint,
+        headers: getHeader(),
+      })
+        .then((res) => {
+          this.topTracksShort = res.data.items;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      const long_term_track_endpoint = `https://api.spotify.com/v1/me/top/tracks?time_range=long_term&offset=0&limit=${TOP_COUNT}`;
+      axios({
+        method: "GET",
+        url: long_term_track_endpoint,
+        headers: getHeader(),
+      })
+        .then((res) => {
+          this.topTracksLong = res.data.items;
         })
         .catch((err) => {
           console.error(err);
         });
     },
+    changeTimeRange(range) {
+      switch (range) {
+        case "short_term":
+          this.curTopTracks = this.topTracksShort;
+          break;
+        case "long_term":
+          this.curTopTracks = this.topTracksLong;
+          break;
+        case "medium_term":
+        default:
+          this.curTopTracks = this.topTracksMedium;
+          break;
+      }
+    },
   },
   /*
+    TODO: Consider handling all api calls in the back-end
     handleTopTracks() {
       const path = BACK_END_URL + "/spotlight/toptracks/" + TOP_COUNT;
       axios({
