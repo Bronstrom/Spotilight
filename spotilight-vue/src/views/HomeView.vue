f47953d795652d4458b59bc0e13e288a4857d523
Bronstrom <bafellstrom@gmail.com>
2024-02-20 22:17:50 -0500

    Swap Vue logo appearances & favicon for brand logo

--- spotilight-vue/src/views/HomeView.vue
+++ spotilight-vue/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" />
+    <img alt="Vue logo" src="@/assets/Spotilight_Logo.png" height="250" />
     <Login welcome_msg="Welcome to Spotilight!" />
   </div>
 </template>
