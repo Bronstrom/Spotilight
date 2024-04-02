40b83e5754a7a79f30da0238f66696881dc3899c
Bronstrom <bafellstrom@gmail.com>
2024-04-02 00:19:22 -0400

    Add to About description page

--- spotilight-vue/src/views/AboutView.vue
+++ spotilight-vue/src/views/AboutView.vue
@@ -1,6 +1,372 @@
 <template>
-  <div class="about">
-    <h1>About Spotilight</h1>
-    <p>Spotilight is . . .</p>
+  <div class="about main-content-gutter margin-top-bottom">
+    <h1 class="text-align-center">About Spotilight</h1>
+
+    <p>
+      Spotilight is a tool for intuitively managing your playlists and
+      visualizing your music taste. Ever curious about who your favorite artist
+      or your top genre is at a period of time? Ever wanted to combine two
+      playlists together to make a new playlist. Spotilight has that all here!
+    </p>
+
+    <h1>Getting Started with Spotilight</h1>
+
+    <h2 id="authentification">Authentification</h2>
+    <hr />
+
+    <p>
+      When first navigating to Spotilight's landing page in your web browser,
+      you will be greeted to a page asking you to authenticate to Spotilight.
+      When first clicking the "Authenticate with Spotify" button, you will be
+      directed to a Spotify sign-in page if you are not currently signed into
+      Spotify on your browser. Provide your Spotify credentials and click "Sign
+      In". Next your will be asked if you will allow for Spotilight to "view
+      your Spotify account data", "view your activity on Spotify", and "take
+      actions in Spotify on your behalf". These account access rights will be
+      required in order to use the Spotilight app. This allows for the
+      application to manage your playlists in a more interactive way, as well as
+      view your top stats. If you agree to give Spotilight these access rights
+      click "Agree". If you ever want to remove Spotilight's access to your
+      account, you can do so from
+      <a href="https://www.spotify.com/us/account/apps/"
+        >Spotify's Account Manage Apps page</a
+      >. You have now successfully authentificated into the application! You
+      will be welcomed to the app with your account display name.
+    </p>
+
+    <h2>Playlists</h2>
+    <hr />
+
+    <p>
+      The "Playlists" page gives you a comprehensive way to view and manage your
+      own playlists. The features given on the Playlists page gives you a robust
+      set of features to manage your playlists not available on Spotify.
+    </p>
+
+    <h3>Viewing and Managing Multiple Playlists</h3>
+
+    <p>
+      To navigate to the "List Playlist" page where you can view all of your
+      playlists, click on the "Playlists" tab within the top navigation.
+    </p>
+
+    <h4>Viewing (Playlists)</h4>
+
+    <p>
+      There are two ways you can view your playlists, this is through the "Grid"
+      and "List" views. In "Grid" view, you can view your playlists in a card
+      block format. In "List" view, you can view your playlists in a tabular row
+      format.
+    </p>
+
+    <h4>Sorting (Playlists)</h4>
+
+    <p>
+      You have a few options to sort your playlists, which organize how you view
+      your playlists. Select the "Sort" dropdown to choose one of the following
+      options:
+    </p>
+
+    <h5>Newest First</h5>
+
+    <p>Sort playlist ordered by the user's most recently created playlists.</p>
+
+    <h5>Oldest First</h5>
+
+    <p>Sort playlist ordered by the user's least recently created playlists.</p>
+
+    <h5>Title Ascending</h5>
+
+    <p>Sort playlists alphabetically by title.</p>
+
+    <h5>Title Descending</h5>
+
+    <p>Sort playlists reverse alphabetically by title.</p>
+
+    <h5>Owner</h5>
+
+    <p>Sort playlists by owner display name aphabetically</p>
+
+    <h5>Total Tracks</h5>
+
+    <p>Sort playlists by the total number of tracks from highest to lowest.</p>
+
+    <h4>Filtering (Playlists)</h4>
+
+    <p>
+      You have a few options to filter your playlists, which show and hide
+      playlists from the playlist list page. Select the "Filter" dropdown to
+      choose one of the following options:
+    </p>
+
+    <h5>No Filter</h5>
+
+    <p>Show all of your playlists.</p>
+
+    <h5>Public Playlists Only</h5>
+
+    <p>View playlists you are not showing on your public profile.</p>
+
+    <h5>Private Playlists Only</h5>
+
+    <p>View the playlists you share on your public profile.</p>
+
+    <h4>Selection mode (Playlists)</h4>
+
+    <p>
+      When clicking on the "Selection Mode" button, you will be able to click on
+      one or more playlists and get a couple options on ways you can manage or
+      alter you playlists.
+    </p>
+
+    <h5>Delete Selected Items</h5>
+
+    <p>
+      When selecting one or more playlists, you have the option to delete the
+      items you have selected. A modal will appear to confirm when clicking this
+      button. You can retrieve any deleted playlists in a certain timeline
+      specified by Spotify from this link:
+      https://www.spotify.com/us/account/recover-playlists/.
+    </p>
+
+    <h5>Duplicate Playlist</h5>
+
+    <p>
+      This option appears when you have selected a single playlist. All the
+      items within your playlist will be added to a new playlist. When selecting
+      this option, a modal will appear with a required text field for the name
+      of the new playlist.
+    </p>
+
+    <h5>Merge Selected Items</h5>
+
+    <p>
+      This option appears when you have selected more than one playlist. All the
+      items within the playlists you have selected will be combined and added to
+      a new playlist without deleting the current playlists you have created.
+      When selecting this option, a modal will appear with a required text field
+      for the name of the new playlist.
+    </p>
+
+    <h3>Viewing and Managing a Single Playlists</h3>
+
+    <p>
+      When not in selection mode and you click on a playlist, you can view more
+      details about an individual playlist and it's track information.
+    </p>
+
+    <h4>Viewing (Tracks)</h4>
+
+    <p>
+      Just like with playlists on the "List Playlists" page, there are two ways
+      you can view a playlists tracks, this is through the "Grid" and "List"
+      views. In "Grid" view, you can view your tracks in a card block format. In
+      "List" view, you can view your tracks in a tabular row format.
+    </p>
+
+    <h4>Sorting (Tracks)</h4>
+
+    <p>
+      You have a few options to sort tracks in a playlist, which organize how
+      you view your tracks in a playlist. Select the "Sort" dropdown to choose
+      one of the following options:
+    </p>
+
+    <h5>Unsorted</h5>
+
+    <p>Sort tracks ordered by the user.</p>
+
+    <h5>Newest First</h5>
+
+    <p>Sort tracks ordered by the user's most recently added tracks.</p>
+
+    <h5>Oldest First</h5>
+
+    <p>Sort tracks ordered by the user's least recently added tracks.</p>
+
+    <h5>Title Ascending</h5>
+
+    <p>Sort tracks alphabetically by title.</p>
+
+    <h5>Title Descending</h5>
+
+    <p>Sort tracks reverse alphabetically by title.</p>
+
+    <h5>Lead Artist</h5>
+
+    <p>Sort tracks ordered by the lead artist alphabeetically.</p>
+
+    <h4>Filtering (Tracks)</h4>
+
+    <p>
+      You have a few options to filter tracks in a playlist, which show and hide
+      tracks from a playlist's details page. Select the "Filter" dropdown to
+      choose one of the following options:
+    </p>
+
+    <h5>No Filter</h5>
+
+    <p>Show all of the tracks in the current playlist.</p>
+
+    <h5>No Local Files Only</h5>
+
+    <p>Hide local track files in the current playlist.</p>
+
+    <h5>Local Files Only</h5>
+
+    <p>Show only locally added tracks in the current playlist.</p>
+
+    <h5>No Explicits Only</h5>
+
+    <p>Hide tracks with explicit words in them in the current playlist.</p>
+
+    <h5>Explicits Only</h5>
+
+    <p>Show only tracks with explicit words in them in the current playlist.</p>
+
+    <h4>Selection mode (Tracks)</h4>
+
+    <p>
+      When clicking on the selection mode button, you will be able to click on
+      one or more tracks and get a couple options on ways you can manage or
+      alter tracks in a playlist.
+    </p>
+
+    <h5>Select All Items</h5>
+
+    <p>Select all tracks in the current playlist.</p>
+
+    <h5>Unselect All Items</h5>
+
+    <p>Unselect all tracks in the current playlist.</p>
+
+    <h5>Delete Selected Items</h5>
+
+    <p>
+      When selecting one or more tracks, you have the option to delete the items
+      you have selected from the current playlist. A modal will appear to
+      confirm when clicking this button.
+    </p>
+
+    <h5>Create Playlist from Selected Items</h5>
+
+    <p>
+      This option appears when you have selected one or more tracks. All the
+      items within the playlist you have selected will be added to a new
+      playlist. When selecting this option, a modal will appear with a required
+      text field for the name of the new playlist.
+    </p>
+
+    <h2>Showcase/Stats</h2>
+    <hr />
+
+    <p>
+      View statistics and information about your listening history by going to
+      the "Showcase" tab.
+    </p>
+
+    <h3>Top Tracks</h3>
+
+    <p>
+      To view your top tracks for a specific time frame you can view them within
+      the "Top Tracks" section of the page.
+    </p>
+
+    <h4>Top Tracks Configurations</h4>
+
+    <p>
+      You have the option to view Short Term (Past 4 Weeks), Medium Term (Past 6
+      Moths), and Long Term (Past Few Years) durations for you top tracks by
+      selecting the time frame dropdown. You can also display a limited number
+      of top tracks (3, 5, 10, and 25) on a page by choosing the count dropdown.
+    </p>
+
+    <h4>Create a playlist directly from Top Tracks</h4>
+
+    <p>
+      Click the "+ Create Playlist" button to create a playlist from the tracks
+      currently displayed.
+    </p>
+
+    <h3>Top Artists</h3>
+
+    <p>
+      To view your top artists for a specific time frame you can view them
+      within the "Top Artists" section of the page.
+    </p>
+
+    <h4>Top Tracks Configurations</h4>
+
+    <p>
+      You have the option to view Short Term (Past 4 Weeks), Medium Term (Past 6
+      Moths), and Long Term (Past Few Years) durations for you top artists by
+      selecting the time frame dropdown. You can also display a limited number
+      of top artists (3, 5, 10, and 25) on a page by choosing the count
+      dropdown.
+    </p>
+
+    <h4>Create a playlist directly from Top Tracks</h4>
+
+    <p>
+      Click the "+ Create Playlist" button to create a playlist from the top 5
+      tracks for the artists currently displayed. Please note, if there are a
+      lot of artists displayed it may take up to 10 seconds for the full request
+      to process.
+    </p>
+
+    <h3>Top Genres</h3>
+
+    <p>
+      In the "Top Genres" section of the page, you can view a breakdown in a
+      doughnut view of your top genres and sub-genres (smaller categorizations
+      of genres) you listen to for a period of time.
+    </p>
+
+    <h4>Top Genres Configurations</h4>
+
+    <p>
+      You have the option to view Short Term (Past 4 Weeks), Medium Term (Past 6
+      Moths), and Long Term (Past Few Years) durations for you top artists by
+      selecting the time frame dropdown.
+    </p>
+
+    <h2>About</h2>
+    <hr />
+
+    <p>
+      You can view information on how to use the application and how to use it
+      on this page.
+    </p>
+
+    <h2>Logging in/out of your account</h2>
+    <hr />
+
+    <p>
+      In the top application navigation bar, you have the ability to login or
+      logout of your account by clicking on the circular profile icon. You are
+      signed in if your own Spotify profile icon appears in the circle;
+      otherwise, a placeholder user icon will appear within the circle
+      indicating no user is logged in. Once clicking on the circular icon menu
+      options will dropdown. If you are logged in, you will have the "Logout"
+      option, if you are not logged in, you will have the "Login" option. When
+      clicking on "Login", you go through a similar flow to
+      <a href="#authentification">Authentification</a>. When clicking on
+      "Logout", you will be directly logged out of Spotify and met with a screen
+      saying "You are logged out."
+    </p>
   </div>
 </template>
+
+<style>
+.about {
+  text-align: left;
+}
+
+.about h1,
+h2,
+h3,
+h4,
+h5 {
+  padding-top: 2rem;
+}
+</style>
